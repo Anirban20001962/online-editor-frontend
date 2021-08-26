ee88ba5630b17d8596a17d8eb6526094c37c8868
RanjanKumar666 <typhonazazel@gmail.com>
2021-08-26 09:05:12 +0530

    minor change

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -10,54 +10,54 @@ import { motion } from 'framer-motion';
 const MotionBox = motion(Box);
 
 const Terminal = ({ colors }) => {
-	const [output, setOutput] = useState('');
-	useEffect(() => {
-		socket.on(EVENTS.OUTPUT, (data) => {
-			setOutput(data);
-		});
-	}, [colors]);
+    const [output, setOutput] = useState('');
+    useEffect(() => {
+        socket.on(EVENTS.OUTPUT, (data) => {
+            setOutput(data);
+        });
+    }, [colors]);
 
-	return (
-		// showOutput && (
-		<MotionBox
-			color={colors.foreground}
-			bg={colors.background}
-			width="100%"
-			// position="absolute"
-			bottom="0"
-			drag="y"
-			height="350px"
-			overflow="hidden"
-			dragConstraints={{
-				top: -200,
-				bottom: 0,
-			}}
-			dragMomentum={false}
-			marginTop="-1"
-			dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
-			dragElastic={0}
-		>
-			<MotionBox
-				textAlign="left"
-				px="4"
-				py="2"
-				bg={colors.lineHighlightBackground}
-				whileHover={{
-					cursor: 'n-resize',
-				}}
-			>
-				<Heading as="h4" fontSize="lg" fontWeight="medium">
-					Output
-				</Heading>
-			</MotionBox>
-			<Box px="4">
-				{/* <Textarea readOnly> */}
-				{'>'} {output}
-				{/* </Textarea> */}
-			</Box>
-		</MotionBox>
-		// )
-	);
+    return (
+        // showOutput && (
+        <MotionBox
+            color={colors.foreground}
+            bg={colors.background}
+            width="100%"
+            // position="absolute"
+            bottom="0"
+            drag="y"
+            height="350px"
+            overflow="hidden"
+            dragConstraints={{
+                top: -200,
+                bottom: 0,
+            }}
+            dragMomentum={false}
+            marginTop="-1"
+            dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
+            dragElastic={0}
+        >
+            <MotionBox
+                textAlign="left"
+                px="4"
+                py="2"
+                bg={colors.lineHighlightBackground}
+                whileHover={{
+                    cursor: 'n-resize',
+                }}
+            >
+                <Heading as="h4" fontSize="lg" fontWeight="medium">
+                    Output
+                </Heading>
+            </MotionBox>
+            <Box px="4">
+                {/* <Textarea readOnly> */}
+                {'>'} {output}
+                {/* </Textarea> */}
+            </Box>
+        </MotionBox>
+        // )
+    );
 };
 
 export default Terminal;
