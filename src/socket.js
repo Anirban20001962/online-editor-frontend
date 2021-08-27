f034f34c89f97312483057e62d3d850b2c573d70
RanjanKumar666 <typhonazazel@gmail.com>
2021-08-28 01:54:09 +0530

    chnages .env and socket

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,9 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000/');
+const socket = io();
 
 socket.on('connect', () => {
-	console.log('socket connection made');
+    console.log('socket connection made');
 });
 
 export default socket;
