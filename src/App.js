d6081dabd438cdc8191b54d6be467e3c448ab684
Anirban20001962 <anirbandhakuria@gmail.com>
2021-08-29 19:31:49 +0530

    next line

--- src/App.js
+++ src/App.js
@@ -9,87 +9,86 @@ import { Box, Spinner, Container } from '@chakra-ui/react';
 import { useEditorTheme } from './hooks';
 
 function App() {
-    const [language, setLanguage] = useState('javascript');
-    const [content, setContent] = useState(LANGUAGES[language].initialContent);
-    const [theme, setTheme] = useState(DEFAULT_THEME);
-    const [colors, setColors] = useState();
-    const [fontSize, setFontSize] = useState(18);
-    const [themesAvaliable, colorsAvaliable] = useEditorTheme();
-    const editorRef = useRef(null);
+	const [language, setLanguage] = useState('javascript');
+	const [content, setContent] = useState(LANGUAGES[language].initialContent);
+	const [theme, setTheme] = useState(DEFAULT_THEME);
+	const [colors, setColors] = useState();
+	const [fontSize, setFontSize] = useState(18);
+	const [themesAvaliable, colorsAvaliable] = useEditorTheme();
+	const editorRef = useRef(null);
 
-    const handleEditorDidMount = (_editor, monaco) => {
-        for (let theme in themesAvaliable) {
-            monaco.editor.defineTheme(theme, themesAvaliable[theme]);
-        }
-        monaco.editor.setTheme(DEFAULT_THEME);
-        setColors(colorsAvaliable[DEFAULT_THEME]);
-        editorRef.current = monaco;
-    };
+	const handleEditorDidMount = (_editor, monaco) => {
+		for (let theme in themesAvaliable) {
+			monaco.editor.defineTheme(theme, themesAvaliable[theme]);
+		}
+		monaco.editor.setTheme(DEFAULT_THEME);
+		setColors(colorsAvaliable[DEFAULT_THEME]);
+		editorRef.current = monaco;
+	};
 
-    const updateTheme = (e) => {
-        const value = e.target.value;
-        setTheme(value);
-        setColors(colorsAvaliable[value]);
-    };
+	const updateTheme = (e) => {
+		const value = e.target.value;
+		setTheme(value);
+		setColors(colorsAvaliable[value]);
+	};
 
-    const runCode = () => {
-        console.log('running');
-        socket.emit(EVENTS.RUN, {
-            content,
-            language,
-            ext: LANGUAGES[language].ext,
-        });
-    };
+	const runCode = () => {
+		socket.emit(EVENTS.RUN, {
+			content,
+			language,
+			ext: LANGUAGES[language].ext,
+		});
+	};
 
-    if (!colorsAvaliable || !themesAvaliable) {
-        return (
-            <Container
-                width="100vw"
-                height="100vh"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-            >
-                <Spinner size="lg" />
-            </Container>
-        );
-    }
+	if (!colorsAvaliable || !themesAvaliable) {
+		return (
+			<Container
+				width="100vw"
+				height="100vh"
+				display="flex"
+				alignItems="center"
+				justifyContent="center"
+			>
+				<Spinner size="lg" />
+			</Container>
+		);
+	}
 
-    return (
-        <Box width="100%" overflow="hidden" height="100vh">
-            <Box>
-                {colors && (
-                    <Buttons
-                        runCode={runCode}
-                        setLanguage={setLanguage}
-                        setContent={setContent}
-                        updateTheme={updateTheme}
-                        theme={theme}
-                        language={language}
-                        themes={Object.keys(themesAvaliable)}
-                        colors={colors}
-                        fontSize={fontSize}
-                        setFontSize={setFontSize}
-                    />
-                )}
-                <Editor
-                    value={content}
-                    onChange={(value) => setContent(value)}
-                    language={language}
-                    options={{
-                        fontSize,
-                        theme,
-                        fontFamily: 'Inconsolata',
-                    }}
-                    theme={theme}
-                    height="80vh"
-                    loading={<Spinner size="lg" />}
-                    onMount={handleEditorDidMount}
-                />
-            </Box>
-            <Box>{colors && <Terminal colors={colors} />}</Box>
-        </Box>
-    );
+	return (
+		<Box width="100%" overflow="hidden" height="100vh">
+			<Box>
+				{colors && (
+					<Buttons
+						runCode={runCode}
+						setLanguage={setLanguage}
+						setContent={setContent}
+						updateTheme={updateTheme}
+						theme={theme}
+						language={language}
+						themes={Object.keys(themesAvaliable)}
+						colors={colors}
+						fontSize={fontSize}
+						setFontSize={setFontSize}
+					/>
+				)}
+				<Editor
+					value={content}
+					onChange={(value) => setContent(value)}
+					language={language}
+					options={{
+						fontSize,
+						theme,
+						fontFamily: 'Inconsolata',
+					}}
+					theme={theme}
+					height="80vh"
+					loading={<Spinner size="lg" />}
+					onMount={handleEditorDidMount}
+				/>
+			</Box>
+			<Box>{colors && <Terminal colors={colors} />}</Box>
+		</Box>
+	);
 }
 
 export default App;
